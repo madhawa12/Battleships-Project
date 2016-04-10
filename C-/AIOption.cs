6c5e309ae1d452c9be53950ebfec482748069485
AishahMohd <sitiaishahmohd@hotmail.com>
2016-04-10 18:11:41 +0800

    Converted code

---
+++ C-/AIOption.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace MyGame
+{
+	/// <summary>
+	/// The different AI levels.
+	/// </summary>
+	public enum AIOption
+	{
+		/// <summary>
+		/// Easy, total random shooting
+		/// </summary>
+		Easy,
+
+		/// <summary>
+		/// Medium, marks squares around hits
+		/// </summary>
+		Medium,
+
+		/// <summary>
+		/// As medium, but removes shots once it misses
+		/// </summary>
+		Hard
+	}
+
+}
---
+++ C-/ShipName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace MyGame
+{
+	/// <summary>
+	/// The names of all of the ships in the gam
+	/// </summary>
+	/// <remarks></remarks>
+	public enum ShipName
+	{
+		None = 0,
+		Tug = 1,
+		Submarine = 2,
+		Destroyer = 3,
+		Battleship = 4,
+		AircraftCarrier = 5
+	}
+}
+
